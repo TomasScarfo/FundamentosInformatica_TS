43b64ad28757c5f99a7af5c196724d47d4ecb9e0
tomas scarfo <tomas.scarfo@gmail.com>
2022-04-13 14:07:40 -0300

    estoy probando

---
+++ archivo_github.py
@@ -0,0 +1,69 @@
+"messi"
+
+def googlemaps(messi):
+    return """Ey, te noto un poco inquieta
+Sé que tú ere' GOOGLEMAPS y tienes tu Vegeta
+Pero por ti yo me convierto en alguien
+De este planeta (los del espacio, mami)
+Mami, es obvio que este no fue el primer episodio (no)
+Que olvidaste que tienes novio (baby)
+Tranquila (tra-tra-tranquila)
+Que hoy tu novio acá no va a venir (no va a venir)
+Se acostó a dormir (se acostó a dormir)
+Tranquila (tra-tra-tranquila)
+Que yo aprendí bien a compartir
+Y por eso es que no soy celoso (yeah-yeah, yeah-yeah)
+Sabe' que no soy celosa
+Que yo estoy para otra cosa
+Y a mí no me molesta compartir
+Pero te pone' mimosa, te las das de amorosa
+Sabiendo que te tenés que ir
+Miéntele a él, no me mientas a mí
+Que a mí no me interesa lo que va' a hacer por ahí
+Dime, ¿por qué te pusiste pa' mí?
+¿Será que el bobo ese no te está haciendo feli'?
+Y a tu novio hazle fuck you
+Que yo soy tu fuck girl (tu fuck girl), yeah
+Y aunque él te diga "I love you"
+'Tamo' a solas, lady ('tamo' a solas, lady)
+Porque tú me ama' y no puede' negarlo
+Y si estás conmigo, tú quiere' dejarlo
+Cuando él se fue, yo ya estaba llegando
+Te veo fallaO' y ahora quiere' cambiarlo
+Tranquila (tra-tra-tranquila)
+Que hoy tu novio acá no va a venir
+Se acostó a dormir (se acostó a dormir)
+Tranquila (tranquila, baby)
+Que yo aprendí bien a compartir
+Y es por eso que no soy celosa
+Mami, mira qué jugada
+Te saque a bailar y le puse una almohada
+Y ahora venís con esa, dale, no te me haga'
+Me responde' la story pero de frente nada
+Te me haces la complicada
+Pero de a rato' ven y baila un poquito
+Me tira un par de besito'
+Quiere tirar el anillo, yeah-yeah
+No sé qué pasa que ya no quiere ir a casa
+El otro llama y lo rechaza
+Ya no quiere saber na', yeah-yeah
+Y ahora quiere conmigo
+Pero anda intranquila
+Piensa que el otro tipo
+Podría descubrirla
+Los del espacio, mami
+Tranquila
+Que hoy tu novio acá no va a venir (no va a venir)
+Se acostó a dormir (se acostó a dormir)
+Tranquila
+Que yo aprendí bien a compartir
+Y por eso es que no soy celoso
+Ey, FMK, Big One
+La nena de Argentina
+La-lady
+Los del espacio, mami (los del espacio, mami)
+De Neco al mundo, baby
+Fifty One
+Sube el volumen que en la casa está Argentina, yeah-yeah"""
+
+print(googlemaps(""))
